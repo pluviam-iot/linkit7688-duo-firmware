d220db8de394cca4d2a5e56ef1fed64c02fa9d19
Saulo Matte Madalozzo <saulo.zz@gmail.com>
2018-09-19 16:07:14 -0300

    mock BME and response in one time

--- linkit_duo_BME280/linkit_duo_BME280.ino
+++ linkit_duo_BME280/linkit_duo_BME280.ino
@@ -1,87 +1,107 @@
-#include "Seeed_BME280.h"
-#include <Wire.h>
+//#include "Seeed_BME280.h"
+//#include <Wire.h>
 
-BME280 bme280;
+//BME280 bme280;
 
 #define PIN_WIND_SPEED 9
 #define PIN_WIND_DIRECTION 3
 #define PIN_BATTERY 0
+#define WIND_MILLISECONDS_TO_READ 15000
 
+//vars to use inside IRQs
 volatile unsigned int rain = 0;
 volatile unsigned long lastInterruptTimeRain = 0;
 volatile unsigned long interruptTimeRain = 0;
+
+//wind speed is not inside IRQs
 unsigned int windPulses = 0;
 
 void setup() {
+  //open serial to computer 
+  Serial.begin(9600);
+  Serial.println("START");
+  
+  //open internal serial connection to MT7688 
+  Serial1.begin(9600);
+  
   pinMode(PIN_WIND_SPEED, INPUT);
 
-  /* open internal serial connection to MT7688  */
-  Serial1.begin(9600);
-
-  Serial.begin(9600);
-
-  if(!bme280.init()){
+  /*if(!bme280.init()){
     Serial1.println("Device error!");
-  }
+    Serial.println("Device error!");
+  }*/
 
+  //wait a little to stabilize all sensors
   delay(2000);
+
+  //attach rain interrupt
   attachInterrupt(4, interruptRain, FALLING);
-  Serial.println("START");
+  
 }
 
 void loop() {
   byte commandByte = Serial1.read();
 
+  // respond just when receive 'A' command over Serial1 (internal serial to linkit)
   if (commandByte == 'A') {
-    Serial1.print(bme280.getTemperature());
-  } else if (commandByte == 'B') {
-    Serial1.print(bme280.getHumidity());
-  } else if (commandByte == 'C') {
-    Serial1.print(bme280.getPressure());
-  } else if (commandByte == 'D') {
+    //Serial1.print(bme280.getTemperature());
+    Serial1.print(20.2);
+    Serial1.print(';');
+    //Serial1.print(bme280.getHumidity());
+    Serial1.print(85);
+    Serial1.print(';');
+    //Serial1.print(bme280.getPressure());
+    Serial1.print(1013.25);
+    Serial1.print(';');
     Serial1.print(getRain());
-  } else if (commandByte == 'E') {
+    Serial1.print(';');
     Serial1.print(getWindDirection());
-  } else if (commandByte == 'H') {
-    countWindPulses();
-  } else if (commandByte == 'I') {
+    Serial1.print(';');
     Serial1.print(windPulses);
-    windPulses = 0;
-  } else if (commandByte == 'K') {
+    Serial1.print(';');
     Serial1.print(getBattery());
-  } else if (commandByte == '+') {
-    Serial1.print("ABCDEGK");
-  }
+    Serial1.print('\n');
 
+    /* after response was send, start measuring wind speed for next interaction.
+     * First response after boot up wind speed is always 0
+     */
+    countWindPulses();
+  }
+  //wait use less energy
   delay(100);
 }
 
 void interruptRain() {
+  //remember, millis will be frozen inside that function because its called by an IRQ
   interruptTimeRain = millis();
 
-  // If interrupts come faster than 100ms, assume it's a bounce and ignore
+  // If interrupts come faster than 100ms, assume it's a bounce and ignore it
   if (interruptTimeRain - lastInterruptTimeRain > 1000) {
     rain++;
-    Serial.println("rain");
+    //Serial.println("its raining!!!");
   }
 
   lastInterruptTimeRain = interruptTimeRain;
 }
 
+//return rain and empty var
 unsigned int getRain(){
   unsigned int result = rain;
   rain = 0;
   return result;
 }
 
+// wind direction is an analog pin, so, just read and send.
 int getWindDirection (){
   return analogRead(PIN_WIND_DIRECTION);
 }
 
+// analog, read and send too
 int getBattery (){
   return analogRead(PIN_BATTERY);
 }
 
+// run after a serial response, its just counts all pulses in given interval time.
 void countWindPulses() {
   windPulses = 0;
   unsigned long start = millis();
@@ -95,7 +115,7 @@ void countWindPulses() {
       windPulses ++;
     }
     lastStatus = pinStatus;
-    if (millis() - start > 15000) {
+    if (millis() - start > WIND_MILLISECONDS_TO_READ) {
       timeout = true;
     }
   }
